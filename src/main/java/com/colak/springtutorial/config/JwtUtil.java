049c39a1068586c11e938a626019c153ea1e7762
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-02 16:32:14 +0300

    fix deprecated calls

--- src/main/java/com/colak/springtutorial/config/JwtUtil.java
+++ src/main/java/com/colak/springtutorial/config/JwtUtil.java
@@ -49,11 +49,11 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(SECRET_KEY,Jwts.SIG.HS256)
                 .compact();
     }
 
